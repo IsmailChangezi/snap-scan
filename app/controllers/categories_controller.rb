3d9fded193c3b3b97aa64b7c8817b112274216d9
Ismail Ali <ismailchangezi801@gmail.com>
2023-09-13 20:46:44 +0500

    Add Updates

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,23 @@ class CategoriesController < ApplicationController
     def show
         @category = Category.find_by_id(params[:id])
     end
+    def new
+        @category = Category.new
+    end
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to categories_path
+        else 
+          render 'new'
+        end
+    end
+
+    private
+
+    def category_params
+        params[:category].merge!(user_id: current_user.id.to_s)
+        params.require(:category).permit(:icon, :name, :user_id)
+    end
+   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :payments
     
+    def total_transaction_amount
+        payments.sum(:amount)
+      end
 end
